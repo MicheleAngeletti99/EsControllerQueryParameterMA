9bdf4ded7f98882a64ddb74630b90e0b69b5f6f1
Michele Angeletti <m.angeletti99@gmail.com>
2024-08-23 17:59:48 +0200

    esercizio

---
+++ src/main/java/com/example/escontrollerqueryparameter/EsControllerQueryParameterApplication.java
@@ -0,0 +1,13 @@
+package com.example.escontrollerqueryparameter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsControllerQueryParameterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsControllerQueryParameterApplication.class, args);
+	}
+
+}
